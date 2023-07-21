2b9c36bdf5167816c57989c9ab5ff3c7bb12daba
Anwesh43 <anweshthecool0@gmail.com>
2023-07-21 15:49:57 +0530

    adding style changes

---
+++ drip-test-frontend/src/components/presentational/AddItem.tsx
@@ -0,0 +1,37 @@
+import React, { useRef } from "react";
+import { Task } from "../../typing";
+
+interface AddItemProps {
+    onAdd : (task : Omit<Task, "id">) => void 
+}
+const AddItem : React.FC<AddItemProps> = (props : AddItemProps) => {
+    const titleRef = useRef(null)
+    const descriptionRef = useRef(null)
+    return (
+        <div className="flex justify-center items-center w-full h-full z-50 absolute left-0 top-0 bg-blend-overlay">
+            <dialog open className = "p-20 bg-white shadow-xl p-5 flex flex-col gap-3">
+                <h2>Add New Task:</h2>
+                <label className ="block text-gray-700 text-sm font-bold mb-2">
+                    Title
+                </label>
+                <input ref={titleRef} className = 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'>
+                </input>
+                <label className ="block text-gray-700 text-sm font-bold mb-2">
+                    Description
+                </label>
+                <input ref={descriptionRef} className = 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'>
+                </input>
+                <button className = "bg-transparent rounded-md p-2 text-blue-500 text-sm border border-blue-500" onClick = {() => {
+                    const titleInput : HTMLInputElement = (titleRef.current as unknown) as HTMLInputElement
+                    const descriptionInput : HTMLInputElement = (descriptionRef.current as unknown) as HTMLInputElement
+                    props.onAdd({
+                        title: titleInput.value ,
+                        description: descriptionInput.value
+                    })
+                }}>Add</button>
+            </dialog>
+        </div>
+    )
+}
+
+export default AddItem
--- drip-test-frontend/src/components/presentational/ViewTask.tsx
+++ drip-test-frontend/src/components/presentational/ViewTask.tsx
@@ -1,5 +1,7 @@
-import React, { Fragment } from "react";
+import axios from "axios";
+import React, { Fragment, useState } from "react";
 import { Task } from "../../typing";
+import AddItem from "./AddItem";
 
 interface TaskList {
     todos : Array<Task>
@@ -11,21 +13,33 @@ interface TaskProps {
 
 const TaskComponent : React.FC<TaskProps> = (taskProps : TaskProps) => {
     return (
-        <div className = 'flex p-6 container-md mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
+        <div className = 'flex p-6 container-md w-30 mx-auto bg-white rounded-xl shadow-lg flex-col items-center space-x-4'>
             <div className="text-xl font-medium text-black">{taskProps.task.title}</div>
             <p className="text-slate-500">{taskProps.task.description}</p>
+            <button className = 'bg-transparent rounded-md p-2 text-blue-500 text-sm border border-blue-500' onClick = {async () => {
+                await axios.delete(`http://localhost:4000/tasks/${taskProps.task.id}`)
+            }}>delete</button>
         </div>
     )
 }
 
 
 const TaskList : React.FC<TaskList> = (taskList : TaskList) => {
+    const [showDialog, setShowDialog] = useState<boolean>(false)
     return (
-        <div className = 'container flex flex-row w-100 p-60 items-start'>
-            {taskList.todos.map(t => (
-                <TaskComponent key = {t.id} task = {t}></TaskComponent>
-            ))}
-        </div>
+        <Fragment>
+            <div className = 'container w-full flex flex-col p-10 justify-start items-start gap-10'>
+                <div className="flex justify-center items-center w-full">
+                    <button className="bg-indigo-500 text-white text-3xl rounded-full flex justify-center items-center w-12 h-12" onClick={() => {
+                        setShowDialog(true)
+                    }}>+</button>
+                </div>
+                {taskList.todos.map(t => (
+                    <TaskComponent key = {t.id} task = {t}></TaskComponent>
+                ))}
+            </div>
+            {showDialog && <AddItem onAdd={console.log}></AddItem>}
+        </Fragment>
     )
 }
 
